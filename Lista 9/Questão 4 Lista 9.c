448f5fc67c7615c88b8075c524c3fe70bfbc783f
Daniel Victor <daniel.victor199999@gmail.com>
2018-06-11 15:10:53 -0300

    Atualizei dnv, tava errado
    Deu merda bixo

---
+++ Lista 9/Questão 4 Lista 9.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int fatorial(int i){
+
+	int resultado=1;
+
+	for(;i>1;i--){
+		resultado*=i;
+}
+		
+	return(resultado);
+}
+
+int mensagem(void){
+	int n;
+	printf("Digite o numero que voce quer o fatorial\n");
+	scanf("%i",&n);
+	return(n);
+}
+
+void main(void){
+	
+int resultado,num;
+
+num=mensagem();
+resultado=fatorial(num);
+printf("O seu fatorial foi %i",resultado);
+}
